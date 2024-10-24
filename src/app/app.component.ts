9e0a81d7a51ecf53d7932096d0e3c01f4eb9bd12
JamesAskelson <jamesaskelson630@gmail.com>
2024-10-24 17:56:11 -0500

    angular-forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-forms';
+  name = new FormControl('')
+  password = new FormControl('')
+  email = new FormControl('')
+  age = new FormControl(0)
+
+
+  constructor() {
+    console.log('here', this.name)
+  }
+
+  formSubmit( event: any) {
+    console.log(event)
+    // console.log(event.target.age)
+    console.log('before', this.name)
+    this.name = event.value.name;
+    console.log('after', this.name)
+    this.password = event.value.password;
+    this.email = event.value.email;
+    this.age = event.value.age;
+  }
+
+  getValue(name: any){
+    console.log('getvalue', name)
+  }
 }
